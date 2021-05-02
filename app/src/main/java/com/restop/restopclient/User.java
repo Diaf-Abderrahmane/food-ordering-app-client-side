627862315d7b9e9295216b28066a06f0d99a89ab
TRETEC <hanimedmahmoudi@gmail.com>
2021-05-02 17:03:35 +0100

    Restop Client

--- app/src/main/java/com/restop/restopclient/User.java
+++ app/src/main/java/com/restop/restopclient/User.java
@@ -1,5 +1,15 @@
 package com.restop.restopclient;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class User {
     private String UserName;
     private String Email;
@@ -12,6 +22,11 @@ public class User {
         UserName = userName;
         Email = email;
         ImageName = imageName;
+    }
+
+    public User(String userName, String email) {
+        UserName = userName;
+        Email = email;
     }
 
     public String getUserName() {
@@ -36,6 +51,28 @@ public class User {
 
     public void setImageName(String imageName) {
         ImageName = imageName;
+    }
+
+    public void AddUser(User user,String password){
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(user.getEmail(),password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    String UId = task.getResult().getUser().getUid().toString();
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(UId);
+                    ref.setValue(user);
+                }
+
+            }
+        }
+        );
+    }
+    public void EditUser(User user,String password){
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        FirebaseUser User=auth.getCurrentUser();
+        User.updateEmail(user.getEmail());
+        User.updatePassword(password);
     }
 
 }
