d08b361db0ecc165f0d811672559d282098cb137
Anes-1 <anesnob28@gmail.com>
2021-06-09 20:10:27 +0100

    updating

--- app/src/main/java/com/restop/restopclient/PersonalData.java
+++ app/src/main/java/com/restop/restopclient/PersonalData.java
@@ -237,8 +237,8 @@ public class PersonalData extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if(task.isSuccessful()) {
-                                        alertDialog1.dismiss();
                                         Toast.makeText(PersonalData.this, "Data has been changed", Toast.LENGTH_SHORT).show();
+                                        alertDialog1.dismiss();
                                     }
                                 }
                             });
