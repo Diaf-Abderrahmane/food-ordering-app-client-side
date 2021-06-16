b13433f26e0073932cbfad7f7c230989d840c81d
MoDiaf <a.diaf@esi-sba.dz>
2021-06-16 15:57:00 +0200

    login/sign_up

--- app/src/main/java/com/restop/restopclient/SplashScreen.java
+++ app/src/main/java/com/restop/restopclient/SplashScreen.java
@@ -1,20 +1,34 @@
 package com.restop.restopclient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.restop.restopclient.R;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-    public static int SPLASH_TIME_OUT = 2000;
+    public static int SPLASH_TIME_OUT = 3000;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progressBar = findViewById(R.id.progressBar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        },1000);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
