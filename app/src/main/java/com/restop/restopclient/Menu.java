f096667df335ecff71f78f8a4eb9c7ff8dd9e003
Lokmane01 <itlallouche@gmail.com>
2021-06-17 20:25:10 +0100

    menu__________

--- app/src/main/java/com/restop/restopclient/Menu.java
+++ app/src/main/java/com/restop/restopclient/Menu.java
@@ -1,20 +1,33 @@
 package com.restop.restopclient;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -40,6 +53,13 @@ public class Menu extends Fragment {
 
     TextView textView0=null;
 
+    AlertDialog.Builder alertDialog;
+    Dialog dialog;
+    View viewP;
+    ImageView PopUpOptionImg;
+    TextView PopUpOptionName;
+    TextView PopUpOptionDescription;
+    TextView PopUpOptionPrice;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -104,6 +124,8 @@ public class Menu extends Fragment {
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
+
+
         return view;
     }
     public void Refresh(){
@@ -136,7 +158,28 @@ public class Menu extends Fragment {
             textView0 = textView1;
         }
     }
+    public void PopUpOption(int categoryIndex, int position, Drawable img) {
+        alertDialog = new AlertDialog.Builder(getActivity());
+        viewP = getLayoutInflater().inflate(R.layout.popup_option, null);
+        PopUpOptionImg=viewP.findViewById(R.id.PopUpOption_img);
+        PopUpOptionName=viewP.findViewById(R.id.PopUpOption_name);
+        PopUpOptionDescription=viewP.findViewById(R.id.PopUpOption_description);
 
+        /*LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(PopUpOptionImg.getDrawable().getIntrinsicWidth(), PopUpOptionImg.getDrawable().getIntrinsicWidth());
+        PopUpOptionImg.setLayoutParams(layoutParams);*/
+
+        PopUpOptionPrice=viewP.findViewById(R.id.PopUpOption_price);
+        PopUpOptionName.setText(AllCategories.get(categoryIndex).getAllOptions().get(position).getName());
+        PopUpOptionDescription.setText(AllCategories.get(categoryIndex).getAllOptions().get(position).getDescription());
+        String price = AllCategories.get(categoryIndex).getAllOptions().get(position).getPrice() +" DZD";
+        PopUpOptionPrice.setText(price);
+        if(img!=null)PopUpOptionImg.setImageDrawable(img);
+
+        alertDialog.setView(viewP);
+        dialog = alertDialog.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
     public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private int ViewType=0;
         private int CategoryIndex;
@@ -157,6 +200,7 @@ public class Menu extends Fragment {
         }
 
         public class ViewHolder1 extends RecyclerView.ViewHolder {
+            private final CardView OptionView;
             private final TextView OptionName;
             private final TextView OptionDescription;
             private final TextView OptionPrice;
@@ -164,6 +208,7 @@ public class Menu extends Fragment {
 
             public ViewHolder1(View view) {
                 super(view);
+                OptionView=view.findViewById(R.id.Option_v);
                 OptionName = view.findViewById(R.id.OptionName);
                 OptionDescription = view.findViewById(R.id.OptionDescription);
                 OptionPrice = view.findViewById(R.id.OptionPrice);
@@ -171,6 +216,7 @@ public class Menu extends Fragment {
 
             }
 
+            public CardView getOptionView() { return OptionView;  }
             public TextView getOptionName() { return OptionName;  }
             public TextView getOptionDescription() { return OptionDescription;  }
             public TextView getOptionPrice() { return OptionPrice;  }
@@ -228,6 +274,8 @@ public class Menu extends Fragment {
                     viewHolder1.getOptionDescription().setText(AllCategories.get(CategoryIndex).getAllOptions().get(position).getDescription());
                     String price = AllCategories.get(CategoryIndex).getAllOptions().get(position).getPrice() +" DZD";
                     viewHolder1.getOptionPrice().setText(price);
+
+                    final Bitmap[] Img=new Bitmap[1];
                     Option.getImg(AllCategories.get(CategoryIndex).getAllOptions().get(position).getImgName(), new Option.ImgStatus() {
                         @Override
                         public void isLoaded(Bitmap img) {
@@ -235,9 +283,17 @@ public class Menu extends Fragment {
                             else Option.getImg("default.jpg", new Option.ImgStatus() {
                                 @Override
                                 public void isLoaded(Bitmap img) {
+                                    Img[0] =img;
                                     viewHolder1.getOptionImg().setImageBitmap(img);
                                 }
                             });
+                        }
+                    });
+                    viewHolder1.getOptionView().setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            PopUpOption(CategoryIndex,position,viewHolder1.getOptionImg().getDrawable());
+                            return false;
                         }
                     });
                     break;
